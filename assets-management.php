f856fc5f355717bed251b8ab7f4037b06f2899a3
Sohan Chowdhury <89298094+prosohan7@users.noreply.github.com>
2022-06-21 02:12:59 +0600

    assets management

--- assets-management.php
+++ assets-management.php
@@ -13,14 +13,28 @@
  * Domain Path:       /languages
  */
 
+define( 'ASN_ASSETS_PUBLIC_DIR', plugin_dir_url( __FILE__ )."assets/public/" );
+
 class AssetsNinja{
     function __construct() {
+
+        $this->$version = time();
+
         add_action( 'plugins_loaded', array( $this, 'load_textdomain' ) );
         add_action( 'wp_enqueue_scripts', array( $this, 'load_front_assets' ) );
     }
 
     function load_textdomain() {
         load_plugin_textdomain( 'assetsninja', false, plugin_dir_url(__FILE__)."/languages" );
+    }
+
+    function load_front_assets(){
+        wp_enqueue_style( 'asn-main-css', ASN_ASSETS_PUBLIC_DIR ."css/main.css", null, time() );
+
+        wp_enqueue_script( 'asn-main-js', ASN_ASSETS_PUBLIC_DIR ."js/main.js", array('jquery'), $this->version, true );
+
+
+
     }
 }
 
